622fd5fd56ab94abe0a4e5615181550c227b4c66
Victor Paranhos <victor_paranhos@hotmail.com>
2024-06-11 16:51:22 -0300

    hash

--- src/main/java/br/com/victorparanhos/todolist/controller/IUserRepository.java
+++ src/main/java/br/com/victorparanhos/todolist/controller/IUserRepository.java
@@ -4,7 +4,9 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IUserRepository extends JpaRepository<UserModel, UUID>{
 
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+        UserModel findByNomedoUsuario(String nomedoUsuario);
     
 } 
--- src/main/java/br/com/victorparanhos/todolist/controller/UserController.java
+++ src/main/java/br/com/victorparanhos/todolist/controller/UserController.java
@@ -4,10 +4,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,10 +26,21 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByNomedoUsuario(userModel.getNomedoUsuario());
+
+        if(user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario já existe");
+        }
+        var passwordHashed = BCrypt.withDefaults().hashToString(8, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
+        
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+
     }
+        
     @GetMapping("/{id}")
     public Optional<UserModel> index(@PathVariable UUID id) {
         var userIndex = this.userRepository.findById(id);
