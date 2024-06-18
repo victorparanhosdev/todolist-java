2d342cbef19461e79b387389fdc61fff3bb5186a
victorparanhosdev <victor_paranhos@hotmail.com>
2024-06-18 16:24:11 -0300

    [TO-DO] - Filtro e Task

--- src/main/java/br/com/victorparanhos/todolist/task/TaskController.java
+++ src/main/java/br/com/victorparanhos/todolist/task/TaskController.java
@@ -2,6 +2,11 @@ package br.com.victorparanhos.todolist.task;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +22,10 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public ResponseEntity<?> create(@RequestBody TaskModel taskModel) {
-       var tasks = this.taskRepository.save(taskModel);
+    public ResponseEntity<?> create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID)idUser);
+        var tasks = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(tasks);
     }
     
